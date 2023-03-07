b9e03d03ad3f692482cbc255c27f4e4840a25d40
Siti Anasuha <anasuha.rosli@gmail.com>
2023-03-08 03:14:29 +0800

    add redirect to homepage when wrong parameter

--- app/Exceptions/Handler.php
+++ app/Exceptions/Handler.php
@@ -2,11 +2,38 @@
 
 namespace App\Exceptions;
 
+use Illuminate\Database\Eloquent\ModelNotFoundException;
 use Illuminate\Foundation\Exceptions\Handler as ExceptionHandler;
+use Illuminate\Session\TokenMismatchException;
+use Symfony\Component\HttpKernel\Exception\NotFoundHttpException;
 use Throwable;
 
 class Handler extends ExceptionHandler
 {
+    public function render($request, Throwable $e)
+    {
+        if ($e instanceof ModelNotFoundException) {
+            $e = new NotFoundHttpException($e->getMessage(), $e);
+        }
+
+        if ($e instanceof TokenMismatchException) {
+            return redirect()
+                ->back()
+                ->withInput($request->except('password'))
+                ->withErrors([
+                    'Validation Token was expired. Please try again',
+                ]);
+        }
+
+        // You can add your own exception here
+        // so redirect to the home route
+        if ($e instanceof NotFoundHttpException) {
+            return redirect()->route('homepage');
+        }
+
+        return parent::render($request, $e);
+    }
+
     /**
      * A list of exception types with their corresponding custom log levels.
      *
--- routes/web.php
+++ routes/web.php
@@ -24,7 +24,7 @@ use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('welcome');
-});
+})->name('homepage');
 
 Route::controller(LoginController::class)
     ->as('auth.')
